578b087055da39ace3b66b28d9329c22f2e6cf65
Ashton Chen <Ashton.D.Chen@gmail.com>
2015-12-04 11:46:16 -0800

    Fixed webvew warning

--- RSSReader/Reader/View/WebViewController.swift
+++ RSSReader/Reader/View/WebViewController.swift
@@ -9,31 +9,34 @@
 import UIKit
 
 class WebViewController: UIViewController {
-    @IBOutlet weak var webview: UIWebView!
+    //@IBOutlet weak var webview: UIWebView!
     var url : String = ""
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        //print(url)
-        let requestObj = NSURLRequest(URL: NSURL (string: url)!);
-        webview.loadRequest(requestObj);
-        // Do any additional setup after loading the view.
+        
+        let webView:UIWebView = UIWebView(frame: CGRectMake(0,0, UIScreen.mainScreen().bounds.width, UIScreen.mainScreen().bounds.height))
+        let request = NSURLRequest(URL: NSURL (string: url)!);
+        webView.scalesPageToFit = true
+        webView.loadRequest(request);
+        //webView.delegate = self
+        self.view.addSubview(webView)
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-
+    
     /*
     // MARK: - Navigation
-
+    
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    // Get the new view controller using segue.destinationViewController.
+    // Pass the selected object to the new view controller.
     }
     */
-
+    
 }
